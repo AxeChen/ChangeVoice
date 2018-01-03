5362230e422ce83b5e4e2aaf3d6321bfdeb4bb44
axeChen <1137366723@qq.com>
2018-01-03 12:05:20 +0800

    添加录音文件

--- app/src/main/cpp/changeVoice.cpp
+++ app/src/main/cpp/changeVoice.cpp
@@ -2,6 +2,7 @@
 #include "fmod.hpp"
 #include <unistd.h>
 #include <android/log.h>
+
 using namespace FMOD;
 
 // 定义五种不同的模式
@@ -12,100 +13,106 @@ using namespace FMOD;
 #define MODE_GAOGUAI 4
 #define MODE_KONGLING 5
 
-#define LOGI(FORMAT,...) __android_log_print(ANDROID_LOG_INFO,"axe",FORMAT,##__VA_ARGS__);
-#define LOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR,"axe",FORMAT,##__VA_ARGS__);
+#define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO,"axe",FORMAT,##__VA_ARGS__);
+#define LOGE(FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR,"axe",FORMAT,##__VA_ARGS__);
 extern "C"
 
 JNICALL
 JNIEXPORT void JNICALL Java_com_mg_axechen_changevoice_VoiceTools_changeVoice
-        (JNIEnv *jniEnv, jclass jclass, jstring jstring,jint mode){
+        (JNIEnv *jniEnv, jclass jclass, jstring jstring, jint mode) {
 
     // 初始化fmod
     FMOD::System *system;
     FMOD::System_Create(&system);
+
     Sound *sound;
     // 通道（声音是由多种音效组成）
     Channel *channel;
+    // 音频
     DSP *pDSP;
+    // 速度
     float frequency;
 
-    system->init(32,FMOD_INIT_NORMAL,NULL);
+    system->init(32, FMOD_INIT_NORMAL, NULL);
 
     // 将 jstring转为 char
-    const char *path = jniEnv->GetStringUTFChars(jstring,NULL);
-
-    system->createSound(path,FMOD_DEFAULT,NULL,&sound);
-
-
+    const char *path = jniEnv->GetStringUTFChars(jstring, NULL);
+    system->createSound(path, FMOD_DEFAULT, NULL, &sound);
 
     try {
-        switch (mode){
+        switch (mode) {
             case MODE_NORMAL:
-                LOGI("%s","正常");
-                system->playSound(sound,NULL, false,&channel);
+                LOGI("%s", "正常");
+                system->playSound(sound, NULL, false, &channel);
                 break;
             case MODE_DASHU:
-                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&pDSP);
-                pDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,0.7);
-                system->playSound(sound,NULL, false,&channel);
-                channel->addDSP(0,pDSP);
-                LOGI("%s","正常");
+                LOGI("%s", "大叔");
+                // 设置音调，调低音调
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pDSP);
+                pDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 0.7);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
                 break;
             case MODE_LUOLI:
-                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT,&pDSP);
-                pDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH,3);
-                system->playSound(sound,NULL, false,&channel);
-                channel->addDSP(0,pDSP);
-                LOGI("%s","萝莉");
+                LOGI("%s", "萝莉");
+                // 设置音调，调高音调
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pDSP);
+                pDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 3);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
                 break;
             case MODE_GAOGUAI:
-                LOGI("%s","搞怪");
+                LOGI("%s", "搞怪");
                 system->createDSPByType(FMOD_DSP_TYPE_NORMALIZE, &pDSP);
                 system->playSound(sound, NULL, false, &channel);
                 channel->addDSP(0, pDSP);
+                // 获取速度并加速
                 channel->getFrequency(&frequency);
-                frequency = frequency*1.6;
+                frequency = frequency * 1.6;
                 channel->setFrequency(frequency);
                 break;
             case MODE_JINGSONG:
                 // FMOD_DSP_TYPE_TREMOLO 颤抖
-
+                LOGI("%s", "惊悚");
+                // 设置颤抖
                 system->createDSPByType(FMOD_DSP_TYPE_TREMOLO, &pDSP);
-                pDSP->setParameterFloat(FMOD_DSP_TREMOLO_SKEW,0.5);
-
+                // 设置颤抖的频率
+                pDSP->setParameterFloat(FMOD_DSP_TREMOLO_SKEW, 0.8);
                 system->playSound(sound, NULL, false, &channel);
                 channel->addDSP(0, pDSP);
-                LOGI("%s","惊悚");
+                channel->getFrequency(&frequency);
+                frequency = frequency * 0.5;
+                channel->setFrequency(frequency);
                 break;
             case MODE_KONGLING:
-                LOGI("%s","空灵");
+                LOGI("%s", "空灵");
+                // 设置重复
                 system->createDSPByType(FMOD_DSP_TYPE_ECHO, &pDSP);
-                pDSP->setParameterFloat(FMOD_DSP_ECHO_DELAY,300);
-                pDSP->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK,20);
+                // 设置重复的重复延迟
+                pDSP->setParameterFloat(FMOD_DSP_ECHO_DELAY, 300);
+                pDSP->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK, 20);
                 system->playSound(sound, NULL, false, &channel);
                 channel->addDSP(0, pDSP);
                 break;
-
         }
-
-
         system->update();
         bool isPlaying = true;
         // 需要等待，等声音全部播放完成
         // 检查是否播放完成
-        while (isPlaying){
-            channel ->isPlaying(&isPlaying);
+        while (isPlaying) {
+            channel->isPlaying(&isPlaying);
             usleep(1000);
         }
-    }catch (...){
+    } catch (...) {
+        LOGE("%s", "error");
         goto end;
     }
 
     // 回收内存
     end:
-        system->close();
-        system->release();
-        jniEnv ->ReleaseStringUTFChars(jstring,path);
+    system->close();
+    system->release();
+    jniEnv->ReleaseStringUTFChars(jstring, path);
 
 }
 
--- app/src/main/java/com/mg/axechen/changevoice/MainActivity.java
+++ app/src/main/java/com/mg/axechen/changevoice/MainActivity.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,39 +20,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InputStream inputStream =  getResources().openRawResource(R.raw.voice);
+                    File file = new File(Environment.getExternalStorageDirectory().getPath(),"voice.wav");
+                    if(!file.exists()){
+                        file.createNewFile();
+                    }
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    byte[] buffer = new byte[10];
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    int len = 0;
+                    while ((len=inputStream.read(buffer))!=-1){
+                        outputStream.write(buffer,0,len);
+                    }
+                    byte[] bs = outputStream.toByteArray();
+                    fileOutputStream.write(bs);
+                    outputStream.close();
+                    inputStream.close();
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.i("getPath",e.getMessage());
+                }
+            }
+        }).start();
+
     }
 
     public void mFix(View view) {
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"one.wav";
-        Log.i("getPath", path);
+        String path = Environment.getExternalStorageDirectory().getPath()+File.separator+"voice.wav";
+        Log.i("getPath",path);
         switch (view.getId()) {
             // 普通
             case R.id.btn_normal:
-                VoiceTools.changeVoice(path,0);
+                VoiceTools.changeVoice(path, 0);
                 break;
+            // 萝莉
+            case R.id.btn_luoli:
+                VoiceTools.changeVoice(path, 1);
+                break;// 萝莉
             // 大叔
             case R.id.btn_dashu:
-                VoiceTools.changeVoice(path,2);
+                VoiceTools.changeVoice(path, 2);
+                break;
+            // 惊悚
+            case R.id.btn_jingsong:
+                VoiceTools.changeVoice(path, 3);
                 break;
             // 搞怪
             case R.id.btn_gaoguai:
-                VoiceTools.changeVoice(path,4);
+                VoiceTools.changeVoice(path, 4);
                 break;
-
-             // 惊悚
-            case R.id.btn_jingsong:
-                VoiceTools.changeVoice(path,3);
-                break;
-
             // 空灵
             case R.id.btn_kongling:
-                VoiceTools.changeVoice(path,5);
+                VoiceTools.changeVoice(path, 5);
                 break;
 
-            // 萝莉
-            case R.id.btn_luoli:
-                VoiceTools.changeVoice(path,1);
-                break;
         }
 
     }
